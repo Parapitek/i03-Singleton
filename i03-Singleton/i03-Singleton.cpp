8a701fdbd3dd1fae3500740a2d9c2114878996e1
Rustam <safarov.rust@gmail.com>
2023-02-15 12:40:06 +0700

    Добавьте файлы проекта.

---
+++ i03-Singleton/i03-Singleton.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "ChocolateBoiler.h"
+
+int main()
+{
+    ChocolateBoiler* boiler = ChocolateBoiler::getInstance();
+    boiler->fill();
+    boiler = ChocolateBoiler::getInstance();
+    boiler->fill();
+    boiler->boil();
+    boiler->drain();
+
+    return 0;
+}
+
