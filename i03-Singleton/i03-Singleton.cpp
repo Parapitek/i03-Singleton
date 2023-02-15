43e5b97460aa0b96c656a38aabecc61a3f4a2ccd
Rustam Safarov <safarov.rust@gmail.com>
2023-02-15 12:46:32 +0700

    Update i03-Singleton.cpp

--- i03-Singleton/i03-Singleton.cpp
+++ i03-Singleton/i03-Singleton.cpp
@@ -3,13 +3,7 @@
 
 int main()
 {
-    ChocolateBoiler* boiler = ChocolateBoiler::getInstance();
-    boiler->fill();
-    boiler = ChocolateBoiler::getInstance();
-    boiler->fill();
-    boiler->boil();
-    boiler->drain();
-
+    
     return 0;
 }
 
