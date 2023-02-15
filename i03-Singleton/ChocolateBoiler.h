0f982ad7297980ab7437be65ff3dd8f542e8fd25
Rustam Safarov <safarov.rust@gmail.com>
2023-02-15 12:45:02 +0700

    Update ChocolateBoiler.h

--- i03-Singleton/ChocolateBoiler.h
+++ i03-Singleton/ChocolateBoiler.h
@@ -4,10 +4,7 @@ class ChocolateBoiler
 private:
     bool empty;
     bool boiled;
-    static ChocolateBoiler* uniqueInstance;
-    ChocolateBoiler();
 public:
-    static ChocolateBoiler* getInstance();
     void fill();
     void drain();
     void boil();
